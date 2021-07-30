c2efbe3190cc64d2451d23b3823b0b79930d1930
kaleabbyh <kaleabbyh@gmail.com>
2021-07-29 16:46:22 -0800

    firstcommit

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kalab</title>
+    <link rel="stylesheet" href="style/style.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="topnav" id="myTopnav">
+            <a href="#home" class="active">Welcome</a>
+            <a href="customer_register.php">Register</a>
+            <a href="checkout.php">My Account</a>
+            <a href="cart.php">Go To Cart</a>
+            <a href="checkout.php">Login</a>
+            <a href="checkout.php">adminpanel</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="dropdownIcon">&#9776;</i>
+            </a>
+
+        </div>
+
+        <div class="padding-nav" id="myTopnav1">
+            <!-- padding-nav Finish -->
+            <a class="active" href="index.php" class="navbar-brand home"><img src="images/ecom-store-logoo.png" alt="logo"></a>
+            <a href="index.php">Home</a>
+            <a href="shop.php">Shop</a>
+            <a href="checkout.php">My Account</a>
+            <a href="cart.php">Shopping Cart</a>
+            <a href="contact.php">Contact Us</a>
+            <a href="cart.php">Items In Your Cart</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction1()">
+                <i class="dropdownIcon">&#9776;</i>
+            </a>
+        </div><!-- padding-nav Finish -->
+    </header>
+
+    <!-- slider starts-->
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="admin_area/slides_images/1.jpg" style="width:100%">
+            <div class="text">habeshan dess</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="admin_area/slides_images/2.jpg" style="width:100%">
+            <div class="text">best dress</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="admin_area/slides_images/2.jpg" style="width:100%">
+            <div class="text">your choise</div>
+        </div>
+
+    </div>
+    <br>
+
+    <div style="text-align:center">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+
+
+    <!-- slider ends-->
+
+
+    <!-- container Begin -->
+    <div class="hot">
+        <h2>
+            Our Latest Products
+        </h2>
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/11.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/22.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/33a.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/33b.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/33c.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/44a.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/55a.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <img class="img-responsive" src="admin_area/product_images/66a.jpg" alt="Product 1">
+            <div class="details">
+                <h3><a href="details.php">details </a></h3>
+                <p class="price">300birr</p>
+                <p class="button">
+                    <a href="details.php" class="btn">View Details</a>
+                    <a href="details.php" class="btn">Add To Cart</a>
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+
+    
+
+
+<!-- footer starts -->
+
+    <footer class="footer">
+        <div class="row">
+            <div class="col-3">
+                <h4>Pages</h4>
+                <ul>
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="customer/my_account.php">My Account</a></li>
+                </ul>
+                
+                <h4>User Section</h4>
+                <ul>
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul>
+            </div>
+
+            <div class="col-3">
+                <h4>Top Products Categories</h4>
+                <ul>
+                    <li><a href="#">Jackets</a></li>
+                    <li><a href="#">Accessories</a></li>
+                    <li><a href="#">Coats</a></li>
+                    <li><a href="#">Shoes</a></li>
+                    <li><a href="#">T-Shirts</a></li>
+                </ul>
+                
+            </div>
+
+            <div class="col-3">
+                <h4>Find Us</h4>
+                <p>
+                    <strong>Astu@gmail.com</strong>
+                    <br />0223456767
+                    <br />FSE13@gmail.com
+                    <br /><strong>Astu</strong>
+                </p>
+                <a href="contact.php">Check Our Contact Page</a>
+
+            </div>
+
+
+            <div class="col-3">
+                <h4>Get The News</h4>
+                <p class="text-muted">
+                    Dont miss our latest update products.
+                </p>
+                <form action="" method="post">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="email">
+                        <span class="input-group-btn">
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                        </span>
+                    </div>
+                </form>
+                <h4>Keep In Touch</h4>
+            </div>
+        </div>
+
+        <div id="copyright">
+            <div class="container">
+                <div class="col-md-6">
+                    <p class="pull-left">&copy; 2021 ASTU Store All Rights Reserve</p>
+                </div>
+                <div class="col-md-6">
+                
+                </div>
+            </div>
+        </div>
+
+
+        
+    </footer>
+
+
+
+    <script src="js/myjs.js"></script>
+</body>
+
+</html>
